9259a00bcc061e6c1e270f6906d76cce63690379
Quan Nguyen <110146253+qpmng@users.noreply.github.com>
2022-08-23 17:24:54 +0700

    Subcategories distribution reseller

---
+++ SQLqueries/SubcategoriesCountReseller.sql
@@ -0,0 +1,29 @@
+SELECT 
+	EnglishProductCategoryName,
+	EnglishProductSubcategoryName,
+	COUNT(EnglishProductSubcategoryName) AS NoCategoriesPurchasedReseller
+FROM 
+	(
+		SELECT
+			ProductKey,
+			COUNT(ProductKey) AS NumberOfProductsPurchased
+		FROM
+			AdventureWorksDW2019.dbo.FactResellerSales
+		GROUP BY
+			ProductKey
+	) AS TotalNoProducts
+	LEFT JOIN
+		AdventureWorksDW2019.dbo.DimProduct
+		ON TotalNoProducts.ProductKey=AdventureWorksDW2019.dbo.DimProduct.ProductKey
+	LEFT JOIN
+		AdventureWorksDW2019.dbo.DimProductSubcategory
+		ON AdventureWorksDW2019.dbo.DimProduct.ProductSubcategoryKey
+		=AdventureWorksDW2019.dbo.DimProductSubcategory.ProductSubcategoryKey
+	LEFT JOIN
+		AdventureWorksDW2019.dbo.DimProductCategory
+		ON AdventureWorksDW2019.dbo.DimProductSubcategory.ProductCategoryKey
+		=AdventureWorksDW2019.dbo.DimProductCategory.ProductCategoryKey
+GROUP BY
+	EnglishProductCategoryName, EnglishProductSubcategoryName
+ORDER BY
+	NoCategoriesPurchasedReseller DESC
