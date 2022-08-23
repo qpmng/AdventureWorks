eeb4845aea7eee478a14156c4bb8f069e39e768c
Quan Nguyen <110146253+qpmng@users.noreply.github.com>
2022-08-23 11:45:22 +0700

    Top 5 best-selling products

---
+++ SQLqueries/BestSellingProducts.sql
@@ -0,0 +1,42 @@
+SELECT
+	ProductSales.ProductKey, 
+	EnglishProductName AS ProductName,
+	TotalProductsPurchased
+FROM 
+	(
+		SELECT
+			COALESCE(OnlineSales.ProductKey,ResellerSales.ProductKey) AS ProductKey,
+			CASE
+				WHEN OnlineSales.NoProductsPurchasedOnline IS NULL 
+					THEN ResellerSales.NumberOfProductsPurchased
+				WHEN ResellerSales.NumberOfProductsPurchased IS NULL 
+					THEN OnlineSales.NoProductsPurchasedOnline
+				ELSE OnlineSales.NoProductsPurchasedOnline+ResellerSales.NumberOfProductsPurchased 
+			END AS TotalProductsPurchased
+		FROM 
+			(
+				SELECT
+					ProductKey,
+					COUNT(ProductKey) AS NoProductsPurchasedOnline
+				FROM 
+					AdventureWorksDW2019.dbo.FactInternetSales
+				GROUP BY
+					ProductKey
+			) AS OnlineSales
+			LEFT JOIN
+			(
+				SELECT
+					ProductKey,
+					COUNT(ProductKey) AS NumberOfProductsPurchased
+				FROM 
+					AdventureWorksDW2019.dbo.FactResellerSales
+				GROUP BY
+					ProductKey
+			) AS ResellerSales
+			ON OnlineSales.ProductKey=ResellerSales.ProductKey
+	) AS ProductSales
+	LEFT JOIN AdventureWorksDW2019.dbo.DimProduct
+	ON ProductSales.ProductKey=AdventureWorksDW2019.dbo.DimProduct.ProductKey
+ORDER BY
+	TotalProductsPurchased DESC
+;
